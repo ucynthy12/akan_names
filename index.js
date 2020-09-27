18430fc5d8990b501113261403ba57d92c794f84
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-09-27 23:16:42 +0200

    removed modal div

--- index.js
+++ index.js
@@ -10,48 +10,48 @@ function akanName() {
     else {
         if (gender == 'female') {
             if (weekday == 0) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/sunday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/sunday.jpg\" >"
             }
             else if (weekday == 1) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/monday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/monday.jpg\" >"
             }
             else if (weekday == 2) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/tuesday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/tuesday.jpg\" >"
             }
             else if (weekday == 3) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/wednesday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/wednesday.jpg\" >"
             }
             else if (weekday == 4) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/thursday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/thursday.jpg\" >"
             }
             else if (weekday == 5) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/friday2.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/friday2.jpg\" >"
             }
             else if (weekday == 6) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"female-pic/saturday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"female-pic/saturday.jpg\" >"
             }
 
         } else if (gender == 'male') {
             if (weekday == 0) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/sunday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/sunday.jpg\" >"
             }
             else if (weekday == 1) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/monday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/monday.jpg\" >"
             }
             else if (weekday == 2) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/tuesday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/tuesday.jpg\" >"
             }
             else if (weekday == 3) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/wednesday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/wednesday.jpg\" >"
             }
             else if (weekday == 4) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/thursday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/thursday.jpg\" >"
             }
             else if (weekday == 5) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/friday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=320px src=\"male-pic/friday.jpg\" >"
             }
             else if (weekday == 6) {
-                document.getElementById('answer').innerHTML = "<div class=\"modal-body\"><img width=350px, height=320px src=\"male-pic/saturday.jpg\" ></div>"
+                document.getElementById('answer').innerHTML = "<img width=350px, height=32s0px src=\"male-pic/saturday.jpg\" >"
             }
         }
     }
